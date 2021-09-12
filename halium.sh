ae3751642d72f7cec16fc33b2ad301f2a109ceba
dopaemon <polarisdp@gmail.com>
2021-09-13 01:38:22 +0700

    halium 9.0
    Signed-off-by: dopaemon <polarisdp@gmail.com>

---
+++ halium.sh
@@ -0,0 +1,196 @@
+#!/bin/bash
+#
+# Cronos Build Script V3.2
+# For Exynos7870
+# Coded by BlackMesa/AnanJaser1211 @2019
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Main Dir
+CR_DIR=$(pwd)
+# Define toolchan path
+CR_TC=/tmp/ci/gcc/bin/aarch64-linux-gnu-
+# Define proper arch and dir for dts files
+CR_DTS=arch/arm64/boot/dts
+# Define boot.img out dir
+CR_OUT=$CR_DIR/Helios/Out
+# Presistant A.I.K Location
+CR_AIK=$CR_DIR/Helios/A.I.K
+# Main Ramdisk Location
+CR_RAMDISK_PORT=$CR_DIR/Helios/Treble_custom
+CR_RAMDISK_TREBLE=$CR_DIR/Helios/Treble
+# Compiled image name and location (Image/zImage)
+CR_KERNEL=$CR_DIR/arch/arm64/boot/Image
+# Compiled dtb by dtbtool
+CR_DTB=$CR_DIR/boot.img-dtb
+# Kernel Name and Version
+CR_VERSION=V1.0
+CR_NAME=halium
+# Thread count
+CR_JOBS="$(nproc --all)"
+# Target android version and platform (7/n/8/o/9/p)
+CR_ANDROID=p
+CR_PLATFORM=9.0.0
+# Target ARCH
+CR_ARCH=arm64
+# Current Date
+CR_DATE=$(date +%Y%m%d)
+# Init build
+export CROSS_COMPILE=$CR_TC
+# General init
+export ANDROID_MAJOR_VERSION=$CR_ANDROID
+export PLATFORM_VERSION=$CR_PLATFORM
+export $CR_ARCH
+# J710X Specific
+CR_ANDROID_J710X=n
+CR_PLATFORM_J710X=7.0.0
+##########################################
+# Device specific Variables [SM-J530_2GB (F/G/S/L/K)]
+CR_DTSFILES_J530X="exynos7870-j5y17lte_eur_open_00.dtb exynos7870-j5y17lte_eur_open_01.dtb exynos7870-j5y17lte_eur_open_02.dtb exynos7870-j5y17lte_eur_open_03.dtb exynos7870-j5y17lte_eur_open_05.dtb exynos7870-j5y17lte_eur_open_07.dtb"
+CR_CONFG_J530X=j5y17lte_defconfig
+CR_VARIANT_J530X=J530X
+# Device specific Variables [SM-J530_3GB (Y/YM/FM/GM)]
+CR_DTSFILES_J530M="exynos7870-j5y17lte_sea_openm_03.dtb exynos7870-j5y17lte_sea_openm_05.dtb exynos7870-j5y17lte_sea_openm_07.dtb"
+CR_CONFG_J530M=j5y17lte_defconfig
+CR_VARIANT_J530M=J530Y
+# Device specific Variables [SM-J730X]
+CR_DTSFILES_J730X="exynos7870-j7y17lte_eur_open_00.dtb exynos7870-j7y17lte_eur_open_01.dtb exynos7870-j7y17lte_eur_open_02.dtb exynos7870-j7y17lte_eur_open_03.dtb exynos7870-j7y17lte_eur_open_04.dtb exynos7870-j7y17lte_eur_open_05.dtb exynos7870-j7y17lte_eur_open_06.dtb exynos7870-j7y17lte_eur_open_07.dtb"
+CR_CONFG_J730X=j7y17lte_defconfig
+CR_VARIANT_J730X=J730X
+# Device specific Variables [SM-J710X]
+CR_DTSFILES_J710X="exynos7870-j7xelte_eur_open_00.dtb exynos7870-j7xelte_eur_open_01.dtb exynos7870-j7xelte_eur_open_02.dtb exynos7870-j7xelte_eur_open_03.dtb exynos7870-j7xelte_eur_open_04.dtb"
+CR_CONFG_J710X=j7xelte_defconfig
+CR_VARIANT_J710X=J710X
+# Device specific Variables [SM-J701X]
+CR_DTSFILES_J701X="exynos7870-j7velte_sea_open_00.dtb exynos7870-j7velte_sea_open_01.dtb exynos7870-j7velte_sea_open_03.dtb"
+CR_CONFG_J701X=j7veltesea_defconfig
+CR_VARIANT_J701X=J701X
+# Device specific Variables [SM-G610X]
+CR_DTSFILES_G610X="exynos7870-on7xelte_swa_open_00.dtb exynos7870-on7xelte_swa_open_01.dtb exynos7870-on7xelte_swa_open_02.dtb"
+CR_CONFG_G610X=on7xelteswa_defconfig
+CR_VARIANT_G610X=G610X
+# Device specific Variables [SM-J600X]
+CR_DTSFILES_J600X="exynos7870-j6lte_ltn_00.dtb exynos7870-j6lte_ltn_02.dtb"
+CR_CONFG_J600X=j6lte_defconfig
+CR_VARIANT_J600X=J600X
+# Device specific Variables [SM-A600X]
+CR_DTSFILES_A600X="exynos7870-a6lte_eur_open_00.dtb exynos7870-a6lte_eur_open_01.dtb exynos7870-a6lte_eur_open_02.dtb exynos7870-a6lte_eur_open_03.dtb"
+CR_CONFG_A600X=a6lte_defconfig
+CR_VARIANT_A600X=A600X
+# Script functions
+
+BUILD_ZIMAGE()
+{
+	echo "----------------------------------------------"
+	echo " "
+	echo "Building zImage for $CR_VARIANT"
+	export LOCALVERSION=-$CR_NAME-$CR_VERSION-$CR_VARIANT-$CR_DATE
+	make  $CR_CONFG
+	make -j$CR_JOBS
+	if [ ! -e $CR_KERNEL ]; then
+	exit 0;
+	echo "Image Failed to Compile"
+	echo " Abort "
+	fi
+    du -k "$CR_KERNEL" | cut -f1 >sizT
+    sizT=$(head -n 1 sizT)
+    rm -rf sizT
+	echo " "
+	echo "----------------------------------------------"
+}
+BUILD_DTB()
+{
+	echo "----------------------------------------------"
+	echo " "
+	echo "Building DTB for $CR_VARIANT"
+	# Use the DTS list provided in the build script.
+	# This source does not compile dtbs while doing Image
+	make $CR_DTSFILES
+	./scripts/dtbTool/dtbTool -o $CR_DTB -d $CR_DTS/ -s 2048
+    if [ ! -e $CR_DTB ]; then
+    exit 0;
+    echo "DTB Failed to Compile"
+    echo " Abort "
+    fi
+	rm -rf $CR_DTS/.*.tmp
+	rm -rf $CR_DTS/.*.cmd
+	rm -rf $CR_DTS/*.dtb
+    du -k "$CR_DTB" | cut -f1 >sizdT
+    sizdT=$(head -n 1 sizdT)
+    rm -rf sizdT
+	echo " "
+	echo "----------------------------------------------"
+}
+PACK_PORT_IMG()
+{
+	echo "----------------------------------------------"
+	echo " "
+	echo "Building Boot.img for $CR_VARIANT"
+    # Copy Ramdisk
+    cp -rf $CR_RAMDISK_PORT/* $CR_AIK
+	# Move Compiled kernel and dtb to A.I.K Folder
+	mv $CR_KERNEL $CR_AIK/split_img/boot.img-zImage
+	mv $CR_DTB $CR_AIK/split_img/boot.img-dtb
+	# Create boot.img
+	$CR_AIK/repackimg.sh
+	# Remove red warning at boot
+	echo -n "SEANDROIDENFORCE" » $CR_AIK/image-new.img
+	# Move boot.img to out dir
+	mv $CR_AIK/image-new.img $CR_OUT/$CR_NAME-$CR_VERSION-$CR_DATE-$CR_VARIANT-Treble.img
+    du -k "$CR_OUT/$CR_NAME-$CR_VERSION-$CR_DATE-$CR_VARIANT-Treble.img" | cut -f1 >sizkT
+    sizkT=$(head -n 1 sizkT)
+    rm -rf sizkT
+	$CR_AIK/cleanup.sh
+}
+PACK_TREBLE_IMG()
+{
+	echo "----------------------------------------------"
+	echo " "
+	echo "Building Boot.img for $CR_VARIANT"
+    # Copy Ramdisk
+    cp -rf $CR_RAMDISK_TREBLE/* $CR_AIK
+	# Move Compiled kernel and dtb to A.I.K Folder
+	mv $CR_KERNEL $CR_AIK/split_img/boot.img-zImage
+	mv $CR_DTB $CR_AIK/split_img/boot.img-dtb
+	# Create boot.img
+	$CR_AIK/repackimg.sh
+	# Remove red warning at boot
+	echo -n "SEANDROIDENFORCE" » $CR_AIK/image-new.img
+    echo " "
+	# Move boot.img to out dir
+	mv $CR_AIK/image-new.img $CR_OUT/$CR_NAME-$CR_VERSION-$CR_DATE-$CR_VARIANT-Treble.img
+    du -k "$CR_OUT/$CR_NAME-$CR_VERSION-$CR_DATE-$CR_VARIANT-Treble.img" | cut -f1 >sizkT
+    sizkT=$(head -n 1 sizkT)
+    rm -rf sizkT
+	$CR_AIK/cleanup.sh
+}
+# Main Menu
+clear
+echo "----------------------------------------------"
+echo "$CR_NAME $CR_VERSION Build Script"
+echo "----------------------------------------------"
+echo "Starting $CR_VARIANT_G610X kernel build..."
+CR_VARIANT=$CR_VARIANT_G610X
+CR_CONFG=$CR_CONFG_G610X
+CR_DTSFILES=$CR_DTSFILES_G610X
+BUILD_ZIMAGE
+BUILD_DTB
+PACK_PORT_IMG
+echo " "
+echo "----------------------------------------------"
+echo "$CR_VARIANT kernel build finished."
+echo "Compiled DTB Size = $sizdT Kb"
+echo "Kernel Image Size = $sizT Kb"
+echo "Boot Image   Size = $sizkT Kb"
+echo "$CR_OUT/$CR_NAME-$CR_VERSION-$CR_DATE-$CR_VARIANT-Treble.img Ready"
+echo "----------------------------------------------"
